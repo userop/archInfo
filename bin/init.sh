82cf405d4d8553ac24d133255741f0da29328cb6
userop <18665311070@163.com>
2021-12-18 21:32:47 +0800

    更新装机网络配置

--- bin/init.sh
+++ bin/init.sh
@@ -8,6 +8,31 @@ echo y | pacman -S xfce4 xfce4-goodies
 echo y | pacman -S lightdm lightdm-gtk-greeter
 system enable lightdm.service
 
+# 配置DNS
+#@ /etc/resolv.conf
+echo -e "
+# 常见路由
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+nameserver 114.114.114.114
+### 联通网络DNS
+nameserver 202.106.0.20
+"  > /etc/resolv.conf
+
+#@ /etc/hosts
+echo -e "
+127.0.0.1      localhost
+::1            localhost
+127.0.1.1      myhostname.localdomain  myhostname
+" > /etc/hosts
+
+#@ wifi开机自动连接
+#@ /etc/iwd/main.conf
+echo -e "
+[General]
+EnableNetworkConfiguration=true
+" > /etc/iwd/main.conf
+
 # ~/.bashrc
 echo -e "
 #p@自动补全命令
